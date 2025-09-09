ceabd07a4852ea85f5b3634998aa1f8eb2e0d696
Tiago Carmo <tiagohenriqueodoia@gmail.com>
2025-09-09 00:43:23 -0400

    remove duplicated path

--- repo/app/controllers/me_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class MeController < ApplicationController
-  before_action :authenticate_user!
-
-  def show
-    Rails.logger.info("ME#show current_user.id=#{current_user&.id}")
-    render json: current_user.slice(:id, :email, :name, :role)
-  end
-end
