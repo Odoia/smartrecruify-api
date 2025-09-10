b039d0f73387ae8c34e35d7222b2869b99d10bb1
Tiago Carmo <tiagohenriqueodoia@gmail.com>
2025-09-09 23:30:28 -0400

    solve cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
+
 require "rack/cors"
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://localhost:3001"
     resource "*",
       headers: :any,
-      expose:  ["Authorization"],
-      methods: %i[get post put patch delete options head]
+      expose: %w[Authorization],   # expõe o header para o browser ler
+      methods: %i[get post put patch delete options head],
+      credentials: true            # permite cookies (refresh)
   end
 end
